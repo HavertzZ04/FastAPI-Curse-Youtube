8f6fddd32061c090f01827d3c1bb4a271af8557d
HavertzZ <havertzz04@gmail.com>
2025-05-16 21:00:07 -0500

    feat: :art: HTTP best practices
    I added HTTP exceptions

--- main.py
+++ main.py
@@ -1,44 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from data.users import users_list
 from models.user import User
 from utils.user_search import search_user
 
 app = FastAPI()
 
-@app.get("/users")
+@app.get("/users", response_model=list[User])
 async def get_users():
     return users_list
 
 
-@app.get("/user/{id}")
+@app.get("/user/{id}", response_model=User)
 async def get_user(id: int):
     user = search_user(id)
-    if user:
-        return user
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
     
 
-@app.post("/user")
+@app.post("/user", response_model=User, status_code=201)
 async def create_user(user: User):
     if search_user(user.id):
-        return {"error": "User already exists"}
+        raise HTTPException(status_code=409, detail="User already exists")
     users_list.append(user)
     return user
 
 
-@app.put("/user")
+@app.put("/user", response_model=User)
 async def update_user(user: User):
     for index, saved_user in enumerate(users_list):
         if saved_user.id == user.id:
             users_list[index] = user
             return user
-    return {"error": "User not found"}
+    raise HTTPException(status_code=404, detail="User not found")
 
 
-@app.delete("/user/{id}")
+@app.delete("/user/{id}", status_code=200)
 async def delete_user(id: int):
     for index, deleted_user in enumerate(users_list):
         if deleted_user.id == id:
             del users_list[index]
             return {"message": "The user has been deleted"}
-    return {"error": "User not found"}
+    raise HTTPException(status_code=404, detail="User not found")
         
